054eb9eb693fc5794aa5e302ea1a9304e63529c1
shaftmind <adaias.almeida@bandtec.com.br>
2020-06-05 02:30:54 -0300

    atuliizado

---
+++ Legendary/site/funtio.js
@@ -0,0 +1,18 @@
+
+function open_modal(){
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById('modal').style.position = 'fixed';
+}
+
+function close_model(){
+    document.getElementById('modal').style.display = 'none';
+}
+
+
+//function barramenu(){
+    //document.getElementById('mycabeca').style.height = "123"
+//}
+
+//function menu_l(){
+  //document.getElementsByTagName("a").style.border-bottom // "orange 5px solid"
+//}
